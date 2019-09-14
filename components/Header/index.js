0beb068a70454bdbf619483c516346d936cddadf
staccats <47672588+staccats@users.noreply.github.com>
2019-09-13 19:40:25 -0600

    Header section complete

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,5 +9,32 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container');
+
+function Header() {
+    const div1 = document.createElement('div');
+    div1.classList.add('header');
+
+    const span1 = document.createElement('span');
+    span1.classList.add('date');
+    span1.textContent = 'March 28, 2019';
+
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Lambda Times';
+
+    const span2 = document.createElement('span');
+    span2.classList.add('temp')
+    span2.textContent = '98°'
+
+    headerContainer.appendChild(div1);
+    div1.appendChild(span1);
+    div1.appendChild(h1);
+    div1.appendChild(span2);
+    
+    console.log(div1)
+}
+
+const newHeader = Header()
+
+
     
